a638288281b155656817fb1fb59f26885bd041aa
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-08-07 15:56:46 -0500

    Update Article.js

--- Article/Article.js
+++ Article/Article.js
@@ -112,9 +112,17 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article
 
 */
+//new elements
 const article =
 document.createElement('div');
 document.createElement('h2');
 document.createElement('p');
+document.createElement('p');
+document.createElement('p');
+document.createElement('p');
 document.createElement('button');
+
  
+function createArticle(title,date,firstParagraph,secondParagraph,thirdParagraph ){
+
+};
